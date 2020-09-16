d51e32916f2ea23385efe18238d1cff94b36c033
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-16 19:57:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,20 +9,20 @@ var refreshDelay = 30000;
 //NB do not include / at the end of url
 var poolsStat = 
 [
-	["pool.cryptopay.org.za", "https://pool.cryptopay.org.za:8119/stats"],
-	["cpa.thepiratemine.nl", "https://cpa.piratepools.nl/:2887/stats"],
-	["miningcache.com", "https://miningcache.com:8119/stats"],
-	["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119/stats"],
-	["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119/stats"],
-        ["mine.cryptopay.org.za", "https://mine.cryptopay.org.za:8119/stats"],
-	["cpa.cnhub.de", "http://cpa.cnhub.de:8127/stats"],
-	["the-miners.de/CPA", "http://207.180.246.163:8113/stats"],
-	["cpacoin.my-mining-pool.de", "https://apicpacoin.my-mining-pool.de/stats"],
-	["cpa.multiminer.us", "https://cpa.multiminer.us:8119/stats"],
-	["cpa.cryptomine.cx", "https://cpa.cryptomine.cx:8001/stats"],
-	["cpa.optimusblue.com","https://cpa.optimusblue.com:8129/stats"],
-	["cpa.youpool.io","http://cpa.youpool.io:8135/stats"],
-	["cpa.romining.farm", "https://cpa.romining.farm/api/pool/stats"]
+	["poolparty.cryptopay.org.za", "https://partystats.cryptopay.org.za/stats"],
+//	["cpa.thepiratemine.nl", "https://cpa.piratepools.nl/:2887/stats"],
+//	["miningcache.com", "https://miningcache.com:8119/stats"],
+//	["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119/stats"],
+//	["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119/stats"],
+//        ["mine.cryptopay.org.za", "https://mine.cryptopay.org.za:8119/stats"],
+//	["cpa.cnhub.de", "http://cpa.cnhub.de:8127/stats"],
+//	["the-miners.de/CPA", "http://207.180.246.163:8113/stats"],
+//	["cpacoin.my-mining-pool.de", "https://apicpacoin.my-mining-pool.de/stats"],
+//	["cpa.multiminer.us", "https://cpa.multiminer.us:8119/stats"],
+//	["cpa.cryptomine.cx", "https://cpa.cryptomine.cx:8001/stats"],
+//	["cpa.optimusblue.com","https://cpa.optimusblue.com:8129/stats"],
+//	["cpa.youpool.io","http://cpa.youpool.io:8135/stats"],
+//	["cpa.romining.farm", "https://cpa.romining.farm/api/pool/stats"]
 ];
 
 var nodesStat = 
