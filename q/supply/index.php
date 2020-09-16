82cda7637bc39d9c9bb63f9d2eedd3dac8dc93eb
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-16 22:07:46 +0200

    Update index.php

--- q/supply/index.php
+++ q/supply/index.php
@@ -1,6 +1,6 @@
 <?php 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, 'https://blocks.cryptopay.org.za/info');
 $result = curl_exec($ch);
